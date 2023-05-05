da86fa3c5b8061a84725aa338e8f5ed54b87558c
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-05 21:29:17 +0200

    Update sorse.py

--- AnonX/plugins/sorse.py
+++ AnonX/plugins/sorse.py
@@ -129,3 +129,31 @@ async def ihd(client: Client, message: Message):
             ] 
         ) 
     )
+    
+@app.on_message(command(["‹ رمزيات شباب","‹ رمزيات شباب"]))
+
+async def ihd(client: Client, message: Message):
+
+    rs = random.randint(39,148)
+
+    url = f"https://t.me/GTTUTY/{rs}"
+
+    await client.send_photo(message.chat.id,url,caption="💕 ¦ تـم اختيـار الصوره لـك",parse_mode="html",
+
+    reply_markup=InlineKeyboardMarkup(
+
+            [
+
+                [
+
+                    InlineKeyboardButton(
+
+                        message.from_user.first_name, url=f"https://t.me/{message.from_user.username}")
+
+                ],
+
+            ]
+
+        )
+
+    )
