ad17397f6e40485019a991de9e9e17fb943875fd
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-06 05:38:40 +0300

    Add files via upload

--- AnonX/plugins/call.py
+++ AnonX/plugins/call.py
@@ -9,12 +9,12 @@ from AnonX.utils.database import get_assistant
 
 @app.on_message(filters.voice_chat_started)
 async def stcall(client: Client, message: Message): 
-      Startt = "تم بدأ محادثه صوتيه"
+      Startt = "\n ᴅᴇᴠ ᴡʜɪsᴋᴇʏتم بدأ محادثه صوتيه"
       await message.reply_text(Startt)
 
 @app.on_message(filters.voice_chat_ended)
 async def encall(client: Client, message: Message): 
-      Enddd = "تم إغلاق المحادثه الصوتيه"
+      Enddd = "\n ᴅᴇᴠ ᴡʜɪsᴋᴇʏتم إغلاق المحادثه الصوتيه"
       await message.reply_text(Enddd)
 
 @app.on_message(filters.voice_chat_members_invited)
