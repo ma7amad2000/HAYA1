820f8474728ffc431636b8703f26decbc8b58b19
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-06 04:55:32 +0300

    Update autoleave.py

--- AnonX/plugins/autoleave.py
+++ AnonX/plugins/autoleave.py
@@ -29,9 +29,9 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOG_GROUP_ID
-                                and chat_id != -1001904354189
-                                and chat_id != -1001904354189
-                                and chat_id != -1001904354189
+                                and chat_id != -1001686672798
+                                and chat_id != -1001840101403
+                                and chat_id != -1001549206010
                             ):
                                 if left == 20:
                                     continue
@@ -70,7 +70,7 @@ async def auto_end():
                 try:
                     await app.send_message(
                         chat_id,
-                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.\n ᴅᴇᴠ ᴡʜɪsᴋᴇʏ",
+                        "» ʙᴏᴛ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴀʟʟʏ ʟᴇғᴛ ᴠɪᴅᴇᴏᴄʜᴀᴛ ʙᴇᴄᴀᴜsᴇ ɴᴏ ᴏɴᴇ ᴡᴀs ʟɪsᴛᴇɴɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
                     )
                 except:
                     continue
