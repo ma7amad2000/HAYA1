f2e72320e581a5069aaa3dcd296ed87d7a5578a6
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-08 05:07:48 +0300

    Update userbot.py

--- AnonX/core/userbot.py
+++ AnonX/core/userbot.py
@@ -48,7 +48,7 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("DevilsHeavenMF")
+                await self.one.join_chat("lN_B_Fl")
                 await self.one.join_chat("FallenAssociation")
             except:
                 pass
@@ -79,7 +79,7 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("DevilsHeavenMF")
+                await self.two.join_chat("lN_B_Fl")
                 await self.two.join_chat("FallenAssociation")
             except:
                 pass
@@ -110,7 +110,7 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("DevilsHeavenMF")
+                await self.three.join_chat("lN_B_Fl")
                 await self.three.join_chat("FallenAssociation")
             except:
                 pass
@@ -141,7 +141,7 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("DevilsHeavenMF")
+                await self.four.join_chat("lN_B_Fl")
                 await self.four.join_chat("FallenAssociation")
             except:
                 pass
@@ -172,7 +172,7 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("DevilsHeavenMF")
+                await self.five.join_chat("lN_B_Fl")
                 await self.five.join_chat("FallenAssociation")
             except:
                 pass
