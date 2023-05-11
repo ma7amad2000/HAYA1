4e27c464dee871eab63812045c5525f9f8782df7
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-12 01:34:46 +0300

    Update تاك.py

--- AnonX/plugins/تاك.py
+++ AnonX/plugins/تاك.py
@@ -33,23 +33,23 @@ from pyrogram.errors import FloodWait
 
 
 
-@app.on_message(command(["المالك", "صاحب الخاربه", "المنشي"]) & filters.group)
-async def gak_owne(client: Client, message: Message):
-      if len(message.command) >= 2:
-         return 
-      else:
-            chat_id = message.chat.id
-            f = "administrators"
-            async for member in client.iter_chat_members(chat_id, filter=f):
-               if member.status == "creator":
-                 id = member.user.id
-                 key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
-                 m = await client.get_chat(id)
-                 if m.photo:
-                       photo = await app.download_media(m.photo.big_file_id)
-                       return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🎃 ¦𝙸𝙳 :`{m.id}`\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
-                 else:
-                    return await message.reply("• " + member.user.mention)
+# @app.on_message(command(["المالك", "صاحب الخاربه", "المنشي"]) & filters.group)
+# async def gak_owne(client: Client, message: Message):
+#       if len(message.command) >= 2:
+#          return 
+#       else:
+#             chat_id = message.chat.id
+#             f = "administrators"
+#             async for member in client.iter_chat_members(chat_id, filter=f):
+#                if member.status == "creator":
+#                  id = member.user.id
+#                  key = InlineKeyboardMarkup([[InlineKeyboardButton(member.user.first_name, user_id=id)]])
+#                  m = await client.get_chat(id)
+#                  if m.photo:
+#                        photo = await app.download_media(m.photo.big_file_id)
+#                        return await message.reply_photo(photo, caption=f"🧞‍♂️ ¦𝙽𝙰𝙼𝙴 :{m.first_name}\n🎯 ¦𝚄𝚂𝙴𝚁 :@{m.username}\n🎃 ¦𝙸𝙳 :`{m.id}`\n💌 ¦𝙱𝙸𝙾 :{m.bio}\n✨ ¦𝙲𝙷𝙰𝚃: {message.chat.title}\n♻️ ¦𝙸𝙳.𝙲𝙷𝙰𝚃 :`{message.chat.id}`",reply_markup=key)
+#                  else:
+#                     return await message.reply("• " + member.user.mention)
                     
                     
    
