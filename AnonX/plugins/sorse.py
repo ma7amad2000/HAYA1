180c84606dc4e51d532daf0f52f234a171e290a4
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-14 04:55:25 +0300

    Update sorse.py

--- AnonX/plugins/sorse.py
+++ AnonX/plugins/sorse.py
@@ -12,26 +12,35 @@ from strings.filters import command
 from AnonX import (Apple, Resso, SoundCloud, Spotify, Telegram, YouTube, app)
 from AnonX import app
 from random import  choice, randint
-
 @app.on_message(
-    command(["سورس من","سورس","السورس","سورسي", "سورس حياه"])
-    & ~filters.edited
+    command(["صورص","سورس","السورس","سورس حياه", "haya"])
 )
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://c.top4top.io/p_2680dmevf1.jpg",
-        caption=f"""╭═★⊷⌯⧼[⌞ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐇𝐀𝐘𝐀⌝](https://t.me/lN_B_Fl)⧽⌯⊶★═╮\n★‹ [⌞ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐇𝐀𝐘𝐀⌝𝐀](https://t.me/lN_B_Fl)\n★‹ [𝐇𝐀𝐘𝐀 𝐌𝐔𝐒𝐈𝐂 『 𝒃𝒐𝒕 ⏎ 』](https://t.me/HAYA01BOT?startgroup=true)\n★‹ [『𝐖𝐇𝐈𝐒𝐊𝐄𝐘 ⏎ 』](https://t.me/lV_P_Nl)\n★‹ [group help](https://t.me/HL_BG)\n╰═★⊷⌯⧼[⌞ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐇𝐀𝐘𝐀⌝](https://t.me/lN_B_Fl)⧽⌯⊶★═╯\n ⍟𝑊𝐸𝐿𝐶𝑂𝑀𝐸 𝑇𝑂 𝑆𝑂𝑈𝑅𝐶𝐸 𝐻𝐴𝑌𝐴 𖠅 """,
+        photo=f"https://b.top4top.io/p_2682mb2f41.jpg",
+        caption=f"""
+  ╭═★⊷⌯[⧼⌞𝑆𝑂𝑈𝑅𝐶𝐸 𝐻𝐴𝑌𝐴⧽](https://t.me/HL_BG)⌯⊶★═╮
+   
+   么  [𝑆𝑂𝑈𝑅𝐶𝐸 𝐻𝐴𝑌𝐴⋆♡](https://t.me/HL_BG) 𝐶𝐻𝐴𝑁𝑁𝐸𝐿
+
+   么  [𝑊𝐻𝐼𝑆𝐾𝐸𝑌⋆♡](https://t.me/bP_bP) 𝐷𝐸𝑉𝐸L𝑂𝑃𝐸𝑅    
+
+   么  [َِ𝑀𝑌𝐺𝑅𝑂𝑈𝑃⋆♡](t.me/HL_BG) 𝐺𝑅𝑂𝑈𝑃 𝐻𝐸L𝑃 
+
+   么  [𝑀𝑌𝐵𝑂𝑇⋆♡](t.me/HAYA01BOT) 𝑀𝑈𝑆𝐼𝐶 𝐵𝑂𝑇
+
+  ╰───么[𝐻𝐴𝑌𝐴](https://t.me/HL_BG)  • ◈ •[𝐻𝐴𝑌𝐴](https://t.me/HL_BG)  么───╯ 
+  
+ ⍟𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼
+""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "『𝐖𝐇𝐈𝐒𝐊𝐄𝐘 ⏎ 』༄►", url=f"https://t.me/lV_P_Nl"), 
+                        "𝓦𝓗𝓘𝓢𝓚𝓔𝓨 ♡", url=f"https://t.me/bP_bP"), 
                 ],[
                     InlineKeyboardButton(
-                        "⌞ 𝐒𝐎𝐔𝐑𝐂𝐄 𝐇𝐀𝐘𝐀⌝⚡️", url=f"https://t.me/lN_B_Fl"),
-                ],[
-                    InlineKeyboardButton(
-                        "𝐀𝐃𝐃 𝐌𝐄💞", url=f"https://t.me/HAYA01BOT?startgroup=true"),
+                        "𝓢𝓞𝓤𝓡𝓒𝓔 𝓗𝓐𝓨𝓐 ♡", url=f"t.me/HL_BG"),
                 ],
 
             ]
