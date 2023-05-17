3b79b7db138247c92f6311050381577971ff4b6f
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-17 04:05:20 +0300

    Update config.py

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/ma7amad2000/HAYA1")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/lN_B_Fl")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/HL_BG")
 SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/HL_BG")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
