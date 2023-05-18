4271a73c96a203e50eabeb09888036c48a990d8f
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-05-18 17:09:11 +0300

    Add files via upload

--- AnonX/utils/decorators/play.py
+++ AnonX/utils/decorators/play.py
@@ -189,6 +189,20 @@ def PlayWrapperCHH(command):
                     caption=_["playlist_1"],
                     reply_markup=InlineKeyboardMarkup(buttons),
                 )
+        if message.sender_chat:
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="How to Fix this? ",
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(
+                _["general_4"], reply_markup=upl
+            )
         if message.command[0][0] == "c" or message.command[0][0] == "#":
             chat_id = await get_cmode(message.chat.id)
             if chat_id is None:
