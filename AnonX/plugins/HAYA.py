7dedba27c9d47a4657ffd6fa58859b2884fafc3b
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-06 09:04:55 +0300

    Update HAYA.py

--- AnonX/plugins/HAYA.py
+++ AnonX/plugins/HAYA.py
@@ -132,7 +132,7 @@ async def yas(client, message):
     usr = await client.get_chat("user_OW")
     name = usr.first_name
     photo = await app.download_media(usr.photo.big_file_id)
-    await message.reply_photo(photo,       caption=f"**⩹━★⊷━⌞  𓏺᥉᥆ᥙᖇᥴᥱ ꫝꪖꪗꪖ ⌝━⊶★━⩺\n\n ¦ᦔꫀꪜ :{name}\n ¦ꪊ𝘴ꫀ𝘳 :@{usr.username}\n ¦Ꭵժ :`{user_OW}`\n ¦ႦᎥ᥆ :{usr.bio}\n\n**⩹━★⊷━⌞  𓏺᥉᥆ᥙᖇᥴᥱ ꫝꪖꪗꪖ ⌝━⊶★━⩺**", 
+    await message.reply_photo(photo, 
     reply_markup=InlineKeyboardMarkup(
            [
                 [
