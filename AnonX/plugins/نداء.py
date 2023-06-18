56cbb1383bd094d895f7e32757e0c51652336b68
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-19 00:21:09 +0200

    Update and rename نداء وزوجني.py to نداء.py

--- AnonX/plugins/نداء.py
+++ AnonX/plugins/نداء.py
@@ -25,22 +25,3 @@ def call_random_member(client, message):
     client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
 
 
-
-@app.on_message(command(['زوجني','ز']))
-def call_random_member(client, message):
-    chat_id = message.chat.id
-    members = [
-        member for member in client.iter_chat_members(chat_id)
-        if not member.user.is_bot
-    ]
-    random_member = random.choice(members)
-    random_member_mention = f"[{random_member.user.first_name}](tg://user?id={random_member.user.id})"
-    random_message = random.choice([
-        f"**• اخترت لك هذا الشخص** {random_member_mention} \n **🙈♥**",
-        f"**• اخترت لك هذا الشخص** \n {random_member_mention} \n **"
-    ])
-    client.send_message(chat_id, random_message, reply_to_message_id=message.message_id, parse_mode='markdown')
-
-
-
-
