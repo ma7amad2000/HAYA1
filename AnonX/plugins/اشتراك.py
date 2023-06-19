000d1301830724b2ccd31487bcf1946b8f1246e4
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-19 17:22:19 +0200

    Update اشتراك.py

--- AnonX/plugins/اشتراك.py
+++ AnonX/plugins/اشتراك.py
@@ -7,7 +7,7 @@ import random
 from AnonX import app
 from strings.filters import command
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from config import CHANNEL,  BOT_TOKEN
+from config import USER_CHANNEL,  BOT_TOKEN
 
 from pyrogram.errors import UserAlreadyParticipant, UserNotParticipant
 
@@ -25,7 +25,7 @@ async def start(client: Client, message: Message):
        user = message.from_user.mention
        do = requests.get(f"https://api.telegram.org/app{BOT_TOKEN}/getChatMember?chat_id=@{CHANNEL}&user_id={message.from_user.id}").text
        if do.count("left") or do.count("Bad Request: user not found"):
-          await message.reply_text(f"**Join [this channel](t.me/{CHANNEL}) first to be able to use the app ✨**", disable_web_page_preview=True,
+          await message.reply_text(f"**Join [this channel](t.me/{USER_CHANNEL}) first to be able to use the app ✨**", disable_web_page_preview=True,
           reply_markup=InlineKeyboardMarkup(
                     [
                         [
