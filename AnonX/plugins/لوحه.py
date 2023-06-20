4027f74fb5c8b0aec83d41a48a11dc8303850e41
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-20 02:11:51 +0200

    Update لوحه.py

--- AnonX/plugins/لوحه.py
+++ AnonX/plugins/لوحه.py
@@ -325,7 +325,7 @@ InlineKeyboardButton("Ch",url=f"https://t.me/{show_devchannel()}")
 
 
 
-@app.on_message(command("start"))
+@app.on_message(command("/start"))
 async def app_start(c:Client,m:Message):
 	do = requests.get(f"https://api.telegram.org/bot{bot_id}/getChatMember?chat_id=@{show_channel()}&user_id={m.from_user.id}").text
 	user = m.from_user.id
